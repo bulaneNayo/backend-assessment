ea29d6ab46c6a85530e4ff32e2fba83b92354591
bulaneNayo <bulane.naeo@bothouniversity.com>
2022-12-01 09:31:07 +0200

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,55 @@
-//app
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+
+const app = express();
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+const BASE_URL = "/api/v1";
+
+//sanitize requests against special chars, some precaution against NoSQL Injection Attacks
+app.use(mongoSanitize());
+
+// PORT
+const listenPort =  5000;
+
+//Middleware
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
+const database = (module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  
+  try {
+    mongoose
+      .connect(
+        "mongodb+srv://bulane:Qxkz3TSXCnckLuTd@cluster0.tfkhwlu.mongodb.net/?retryWrites=true&w=majority",
+        connectionParams
+      )
+      .then(
+        () => {
+          console.log("Database is connected");
+        },
+        (err) => {
+          console.log("Can not connect to the database" + err);
+        }
+      );
+  } catch (error) {
+    console.log("Failed to connect database!");
+  }
+});
+database();
+
+app.listen(listenPort, () => {
+  console.log(`Server running at http://localhost:${listenPort}`);
+});
+
+module.exports = app;
+
